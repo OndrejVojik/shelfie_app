f45198247c142b08c13c2e7c076b372503313779
OndrejVojik <vojik.cz@gmail.com>
2025-12-10 18:30:17 +0100

    Dynamic Routes

--- app/(dashboard)/_layout.jsx
+++ app/(dashboard)/_layout.jsx
@@ -48,6 +48,10 @@ export default function DashboardLayout() {
             />
             )}} 
         />
+        <Tabs.Screen 
+            name="books/[id]"
+            options={{ href: null}} 
+        />
         </Tabs>
     </UserOnly>
   )
